08735321337f1d894d5ca61c5514af7d5de3f410
dev-yamaken-0528 <dev.yamaken.0528@gmail.com>
2018-08-21 17:44:48 +0900

    read jsondata

--- routes/play.js
+++ routes/play.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('play', { title: 'PLAY!' });
+  var readdata = JSON.parse(fs.readFileSync('./data/001.json', 'utf8'));
+  res.render('play', { title: 'PLAY!', readdata: readdata});
 });
 
 module.exports = router;
